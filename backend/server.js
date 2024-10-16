8b88584e1ccda4c497e4cc87167554cbbb65ca00
thanukaMinik <warushavithanathanuka@gmail.com>
2024-10-16 21:17:25 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 require('dotenv').config(); // Load environment variables
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
